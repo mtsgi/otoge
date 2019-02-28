d0e2fac8ddf008e5abfe6d13ff7894bb6538b34f
xakazukinx <xakazukinx.goo@gmail.com>
2019-03-01 03:44:40 +0900

    [add]singletonスクリプト [add]ノーツのインターフェースとか [add] PlayerManager

---
+++ otogame/Assets/_Items/Scripts/_GameSystems/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerManager : SingletonMonoBehaviour<PlayerManager> {
+
+	[Serializable]
+	public class Player
+	{
+		public string PlayerName;
+		public int judgePoint;
+		public int score;	
+	}
+
+	[SerializeField] [Multiline(3)]
+	private string _description = "プレイヤーの情報を登録しておくところ。\n開発中はここ直でいじれるようにしておくけど\n長さを2以上にしても意味ないです";
+	[SerializeField] private Player[] _players = new Player[]
+	{
+		new Player(){PlayerName = "player1",judgePoint = 0,score = 0},
+		new Player(){PlayerName = "player2",judgePoint = 0,score = 0},
+	};
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ otogame/Assets/_Items/Scripts/_RythmSystem/IRythmNote.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OtoFuda.RythmSystem
+{
+    public interface IRythmNote
+    {
+        void getJudge(int judgeNum);
+    }    
+
+}
