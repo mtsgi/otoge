9b271ff485800fd3acbca68634c6ea0bb3f19a70
xakazukinx <xakazukinx.goo@gmail.com>
2019-04-08 20:20:06 +0900

    [add]いろいろ試作したものを追加

---
+++ otogame/Assets/_Items/Scripts/_RythmSystem/Note.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OtoFuda.RythmSystem
+{
+
+	public class Note
+	{
+		//譜面の中で何個目のノーツか
+		public int noteNumber;
+		
+		//ノーツが何番目のレーンにあるか
+		public int lane;
+		
+		//ノーツの種類
+		//0~nで表す。
+		//0がSingle,1がLong,2がOtofuda
+		public int noteTypeNumber;
+		
+	}
+	
+
+}
