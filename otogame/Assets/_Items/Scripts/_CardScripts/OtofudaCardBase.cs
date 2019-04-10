7bbe5ce74b154cbbb00de5bf9a08f918d7a06ca8
xakazukinx <xakazukinx.goo@gmail.com>
2019-04-11 02:47:11 +0900

    [add]音札のカードクラス設計周りの作業途中

---
+++ otogame/Assets/_Items/Scripts/_CardScripts/OtofudaCardBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OtoFuda.Card
+{
+    /// <summary>
+    /// 音札のカードクラス。
+    /// これを継承してカードに効果を持たせる。
+    /// </summary>
+    public class OtofudaCardBase : MonoBehaviour
+    {
+
+        [SerializeField] private OtofudaFlowerType _flowerType;
+        [SerializeField] private OtofudaPointType _pointType;
+    
+        //カードの効果を実行する関数
+        internal virtual void applyEffect()
+        {
+        
+        }
+    
+    }
+}
+
---
+++ otogame/Assets/_Items/Scripts/_CardScripts/OtofudaType.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OtoFuda.Card
+{
+    //音札の種別を管理する列挙型。
+    //属性十二種と強度四段階で区別する。
+    /// <summary>
+    /// 音札の属性
+    /// </summary>
+    enum OtofudaFlowerType
+    {
+        MATSU,
+        UME,
+        SAKURA,
+        FUJI,
+        AYAME,
+        BOTAN,
+        HAGI,
+        SUSUKI,
+        KIKU,
+        MOMIJI,
+        YANAGI,
+        KIRI,
+    }
+    
+    /// <summary>
+    /// 音札の属性情報を月で表したもの
+    /// </summary>
+    enum OtofudaMonthType
+    {
+        January,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER,
+    }
+
+    /// <summary>
+    /// 音札の強度
+    /// 数値はコスト…とか？
+    /// </summary>
+    enum OtofudaPointType
+    {
+        SHINE = 20,
+        SEED = 10,
+        STRIP = 5,
+        KASS = 1,
+    }
+
+
+    public class OtofudaType : MonoBehaviour
+    {
+        internal OtofudaMonthType GetOtofudaMonthFromFlower(OtofudaFlowerType flowerType)
+        {
+            return (OtofudaMonthType) Enum.ToObject(typeof(OtofudaMonthType), (int) flowerType);
+        }
+        
+        internal OtofudaFlowerType GetOtofudaFlowerFromMonth(OtofudaMonthType monthType)
+        {
+            return (OtofudaFlowerType) Enum.ToObject(typeof(OtofudaFlowerType), (int) monthType);
+        }
+        
+        [ContextMenu("test")]
+        private void test()
+        {
+            Debug.Log(GetOtofudaMonthFromFlower(OtofudaFlowerType.SAKURA));
+            Debug.Log(GetOtofudaFlowerFromMonth(OtofudaMonthType.NOVEMBER));
+        }
+    }
+}
+
