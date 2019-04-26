d994d09d73626e64265b66c46e754373832feb61
idady <yuta1118onikisu63@softbank.ne.jp>
2019-04-26 11:51:03 +0900

    MusicManagerに選択した曲が流れる機能を追加

---
+++ otogame/Assets/_Items/Scripts/_RythmSystem/MusicManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using shigeno_EditorUtility;
+using UnityEditor;
+using UnityEngine;
+
+namespace OtoFuda.RythmSystem
+{
+    public class MusicManager : SingletonMonoBehaviour<MusicManager>
+    {
+        private void Start()
+        {
+
+        }
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                AudioSource source = SoundManager.Instance.playSound(0);
+                source.Play();
+            }
+        }
+
+        //サウンド名で指定する場合
+        public void Startmusic(string targetSoundName)
+        {
+            AudioSource source = SoundManager.Instance.playSound(targetSoundName);
+            source.Play();
+        }
+
+        //インデックスで指定する場合
+        public void Startmusic(int targetSoundIdx)
+        {
+            AudioSource source = SoundManager.Instance.playSound(targetSoundIdx);
+            source.Play();
+        }
+
+
+
+    }
+}
