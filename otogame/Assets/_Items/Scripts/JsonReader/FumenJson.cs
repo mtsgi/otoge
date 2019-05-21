60901394b80abb0e3da490b9f4696f53b0fddd64
xakazukinx <xakazukinx.goo@gmail.com>
2019-05-21 17:49:52 +0900

    [add]譜面のJsonからノーツを配置できるようにした

---
+++ otogame/Assets/_Items/Scripts/JsonReader/FumenJson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OtoFuda.Fumen
+{
+    [Serializable]
+    public class FumenInfo
+    {
+        public List<NotesInfo> raku = null;
+        public List<NotesInfo> easy = null;
+        public List<NotesInfo> normal = null;
+        public List<NotesInfo> hard = null;
+        public List<NotesInfo> extra = null;
+    }
+
+
+
+    [Serializable]
+    public class NotesInfo
+    {
+        public int type = 0;
+        public int measure = 0;
+        public int lane = 0;
+        public int position = 0;
+        public int split = 0;
+        public int option = -1;
+        public List<NotesInfo> end = null;
+    }
+
+}
