b629119fe2de7f2ea4c035b8cde60ee93fd39b57
xakazukinx <xakazukinx.goo@gmail.com>
2020-07-15 14:54:56 +0900

    Wip

--- otogame/Assets/_PlayerMovement/PlayerAutoFlickMovement.cs
+++ otogame/Assets/_PlayerMovement/PlayerAutoFlickMovement.cs
@@ -3,8 +3,23 @@ using System.Collections.Generic;
 using OtoFuda.Fumen;
 using UnityEngine;
 
-public class PlayerAutoTapMovement : PlayerMovement
+public class PlayerAutoFlickMovement : PlayerMovement
 {
+    [SerializeField] private HandPositionTracer tracer;
+
+    private Vector3 defaultPos;
+    private Vector3 flickPos;
+    private float flickDistance;
+
+    private void Start()
+    {
+        defaultPos = tracer.transform.position;
+        flickPos = defaultPos;
+        flickPos.x += 0.5f;
+
+        flickDistance = Vector3.Distance(defaultPos, flickPos);
+    }
+
     public override void PlayerMovementCheck()
     {
         for (var i = 0; i < PlayerKeys.Length; i++)
@@ -33,11 +48,24 @@ public class PlayerAutoTapMovement : PlayerMovement
 
         //ここでノーツの種類判定と化すればそれぞれのエフェクトとかの実行ができそう
         var noteType = nextNoteTimingInfo.noteType;
-        if (noteType == 1 || noteType == 2)
+        if (noteType == 3 || noteType == 4)
         {
             if (0.05f > GetDifferentAbs(inputTime, judgeTime))
             {
+                if (noteType == 3)
+                {
+                    float present_Location = (Time.time * 1.0f) / flickDistance;
+                    tracer.transform.position = Vector3.Lerp(defaultPos, flickPos, present_Location);
+                }
+                else
+                {
+                    float present_Location = (Time.time * 1.0f) / flickDistance;
+                    tracer.transform.position = Vector3.Lerp(defaultPos, flickPos, -present_Location);
+                }
+
+
                 _inputManager.laneLight[targetLane].SetActive(true);
+
                 base.InputFunction(targetLane, targetTimings, fumenState);
             }
         }
